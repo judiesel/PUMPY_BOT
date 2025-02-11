e8ed4a16a71e0a826ff253b285164edbeb426427
judiesel <72205139+judiesel@users.noreply.github.com>
2025-02-11 17:15:45 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+import os
+
+API_KEY = os.getenv("COINBASE_API_KEY")
+API_SECRET = os.getenv("COINBASE_API_SECRET")
+API_PASSPHRASE = os.getenv("COINBASE_API_PASSPHRASE")
+
+print("🚀 PUMPY démarre avec des clés sécurisées !")
